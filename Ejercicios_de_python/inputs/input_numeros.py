e60546c719aa550f8c2e01a5a0296254352c03d3
Raul Osorio <rosoriocastro2002@gmail.com>
2025-08-15 17:16:10 -0500

    Ejercios de python

---
+++ Ejercicios_de_python/inputs/input_numeros.py
@@ -0,0 +1,11 @@
+#pedirle un numero al usuario
+numero = input("Digita un numero: ")
+
+#multiplicando el numero (antes debo convertir el numero a int)
+#resultado_entero = int(numero) * 2
+
+#multiplicando el numero (antes debo convertir el numero a float)
+resultado_flotante = float(numero) * 2
+
+#mostrar el resultado
+print(f"El numero multiplicado por 2 es: {resultado_flotante}")
