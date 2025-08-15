2d80ceb2db98f04730f41206bcf71f5e6a250a92
Raul Osorio <rosoriocastro2002@gmail.com>
2025-08-15 01:28:06 -0500

    python

---
+++ Ejercicios_de_python/Operadores/op_aritmeticos.py
@@ -0,0 +1,21 @@
+#suma y resta(+ & -)
+suma = 5 + 3
+resta = 5 - 3
+
+#multi y division(* & /)
+multi = 5 * 3
+division = 5 / 3 #division flotante
+
+#potencia(**)
+potencia = 5 ** 3
+
+#dision baja(//)
+division_baja = 5 // 3 #division entera redondeada abajo
+
+#modulo(%)
+modulo = 5 % 3 #resto de la division
+
+#type de dato
+tipo_de_dato = type(suma) #tipo de dato de la variable suma
+
+#print(suma)
---
+++ Ejercicios_de_python/datos_simples.py
@@ -0,0 +1,25 @@
+# ╔════════════════════════════════════════════╗
+# ║                      PYTHON                ║
+# ╚════════════════════════════════════════════╝
+
+#a = 4
+#b = 2
+#c = a + b
+#print(c)
+
+
+#nombre = "Raúl"
+#edad = 22
+#print(nombre)
+#print(type(nombre))
+#print (edad)
+#print (type(edad))
+
+#"""Hola
+#Buenas tardes
+#Esto es un tipo 
+#de dato de py"""
+#40
+#40.40
+#True
+#False
