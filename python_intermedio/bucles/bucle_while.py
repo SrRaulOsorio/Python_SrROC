982480f28ebbf8e4e57c11f00d2bf17f750104fc
Raul Osorio <rosoriocastro2002@gmail.com>
2025-08-19 14:31:56 -0500

    update

--- python_intermedio/bucles/bucle_while.py
+++ python_intermedio/bucles/bucle_while.py
@@ -1,5 +1,8 @@
+#creando un contador que va a ir incrementandose
 contador = 0
 
+#Mientras se cumpla la condicion el bucle se volvera a repetir.
+#se inicia nuevamente y se verifica la condicion
 while contador < 10:
     print(contador)
     contador+=1
